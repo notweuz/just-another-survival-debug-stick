80c4e5f1f7b75683c7495fbb95f7547146feba4a
notweuz <weuz.main@gmail.com>
2025-10-18 00:07:34 +0400

    Initial build

---
+++ src/main/java/ru/ntwz/survivaldebugstick/SurvivalDebugStick.java
@@ -0,0 +1,10 @@
+package ru.ntwz.survivaldebugstick;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SurvivalDebugStick implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
