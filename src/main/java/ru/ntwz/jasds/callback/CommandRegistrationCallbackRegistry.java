1e5a5c04f2850dadf115b4c9ff3d388e84fd7712
notweuz <weuz.main@gmail.com>
2025-10-18 13:06:59 +0400

    Add commands to check whitelist & blacklist list

--- src/main/java/ru/ntwz/jasds/callback/CommandRegistrationCallbackRegistry.java
+++ src/main/java/ru/ntwz/jasds/callback/CommandRegistrationCallbackRegistry.java
@@ -6,7 +6,11 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
+import ru.ntwz.jasds.config.BlacklistConfig;
 import ru.ntwz.jasds.config.ConfigManager;
+import ru.ntwz.jasds.config.WhitelistConfig;
+
+import java.util.List;
 
 public class CommandRegistrationCallbackRegistry {
     public static void register(Logger logger) {
@@ -15,6 +19,16 @@ public class CommandRegistrationCallbackRegistry {
                     .requires(CommandRegistrationCallbackRegistry::hasPermission)
                     .then(CommandManager.literal("reload")
                             .executes(CommandRegistrationCallbackRegistry::executeConfigReload)
+                    )
+                    .then(CommandManager.literal("whitelist")
+                            .then(CommandManager.literal("list")
+                                    .executes(CommandRegistrationCallbackRegistry::executeWhitelistList)
+                            )
+                    )
+                    .then(CommandManager.literal("blacklist")
+                            .then(CommandManager.literal("list")
+                                    .executes(CommandRegistrationCallbackRegistry::executeBlacklistList)
+                            )
                     )
             );
         }));
@@ -28,6 +42,38 @@ public class CommandRegistrationCallbackRegistry {
     private static int executeConfigReload(CommandContext<ServerCommandSource> ctx) {
         ConfigManager.loadConfig();
         ctx.getSource().sendFeedback(() -> Text.literal("§a[JASDS]§r Config reloaded!"), true);
+        return 0;
+    }
+
+    private static int executeWhitelistList(CommandContext<ServerCommandSource> ctx) {
+        WhitelistConfig config = ConfigManager.getWhitelist();
+        List<String> whitelistBlocks = config.blocks;
+        List<String> whitelistProperties = config.properties;
+
+        String propertiesString = String.join("§r, §e", whitelistProperties);
+        String blocksString = String.join("§r, §e", whitelistBlocks);
+
+        if (whitelistBlocks.isEmpty()) ctx.getSource().sendFeedback(() -> Text.literal("§a[JASDS]§r Blocks whitelist is empty"), true);
+        else ctx.getSource().sendFeedback(() -> Text.literal("§a[JASDS]§r Whitelisted blocks: §e" + blocksString + "§r"), true);
+        if (whitelistProperties.isEmpty()) ctx.getSource().sendFeedback(() -> Text.literal("§a[JASDS]§r Properties whitelist is empty"), true);
+        else ctx.getSource().sendFeedback(() -> Text.literal("§a[JASDS]§r Whitelisted properties: §e" + propertiesString + "§r"), true);
+
+        return 0;
+    }
+
+    private static int executeBlacklistList(CommandContext<ServerCommandSource> ctx) {
+        BlacklistConfig config = ConfigManager.getBlacklist();
+        List<String> blacklistblocks = config.blocks;
+        List<String> blacklistProperties = config.properties;
+
+        String propertiesString = String.join("§r, §e", blacklistblocks);
+        String blocksString = String.join("§r, §e", blacklistProperties);
+
+        if (blacklistblocks.isEmpty()) ctx.getSource().sendFeedback(() -> Text.literal("§a[JASDS]§r Blocks blacklist is empty"), true);
+        else ctx.getSource().sendFeedback(() -> Text.literal("§a[JASDS]§r Blacklisted blocks: §e" + blocksString + "§r"), true);
+        if (blacklistProperties.isEmpty()) ctx.getSource().sendFeedback(() -> Text.literal("§a[JASDS]§r Properties blacklist is empty"), true);
+        else ctx.getSource().sendFeedback(() -> Text.literal("§a[JASDS]§r Blacklisted properties: §e" + propertiesString + "§r"), true);
+
         return 0;
     }
 }
